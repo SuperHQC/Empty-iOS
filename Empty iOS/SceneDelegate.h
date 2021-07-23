50a2919b88332e84723ddfb9d2bf96d18acdb21a
hongqichao <hongqichao@bytedance.com>
2021-07-23 11:39:10 +0800

    Initial Commit

---
+++ Empty iOS/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Empty iOS
+//
+//  Created by bytedance on 2021/7/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Empty iOS/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Empty iOS
+//
+//  Created by bytedance on 2021/7/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
